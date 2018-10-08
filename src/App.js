a659318bee44482046f3a4d438b304dc710c6138
Sarah Currey <curreysarah28@gmail.com>
2018-10-08 17:10:07 -0400

    third step complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import FilmListing from './FilmListing.js';
 import FilmDetails from './FilmDetails.js';
+import TMDB from './TMDB.js';
 
 class App extends Component {
+constructor(props) {
+    super(props);
+    this.props = {
+      films: this.props.films
+    };
+  }
+
   render() {
     return (
       <div className="film-library">
+
      <FilmListing />
 
   <FilmDetails/>
