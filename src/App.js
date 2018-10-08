9522e713f3290099e5f5d2de9f7435ba912c5eb5
Sarah Currey <curreysarah28@gmail.com>
2018-10-08 16:47:08 -0400

    details column up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Filmlist from './film-list.js';
+import FilmDetails from './film-details.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="film-library">
+  <div className="film-list">
+    <h1 className="section-title">FILMS</h1>
+  </div>
+
+  <div className="film-details">
+    <h1 className="section-title">DETAILS</h1>
+  </div>
+</div>
     );
   }
 }
---
+++ src/film-details.js
@@ -0,0 +1,1 @@
+film-details.js
