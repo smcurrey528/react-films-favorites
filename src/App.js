df873ac2c7e1b394b693a0067b8a96f93a20ad3c
Sarah Currey <curreysarah28@gmail.com>
2018-10-08 16:53:52 -0400

    step 2 fully complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import Filmlist from './FilmListing.js';
+import FilmListing from './FilmListing.js';
 import FilmDetails from './FilmDetails.js';
 
 class App extends Component {
   render() {
     return (
       <div className="film-library">
-  <div className="film-list">
-    <h1 className="section-title">FILMS</h1>
-  </div>
+     <FilmListing />
 
-  <div className="film-details">
-    <h1 className="section-title">DETAILS</h1>
+  <FilmDetails/>
   </div>
-</div>
     );
   }
 }
