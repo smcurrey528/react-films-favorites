21aacb357a2971c2a655dd0e03c104f53f18f1c4
Sarah Currey <curreysarah28@gmail.com>
2018-10-08 18:27:23 -0400

    step 5 complete

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -3,10 +3,18 @@ import React, {Component} from 'react';
 
 class FilmListing extends Component {
   render() {
+     let allFilms = this.props.film.map((film,index) => {
+     return (
+       <div>
+       <h1>{film.title}</h1>
+       </div>
+       )
+   })
     return(
       <div className="film-list">
     <h1 className="section-title">FILMS</h1>
     <h1> {this.props.film[0].title} </h1>
+    <ul> {allFilms} </ul>
   </div>
   )
   }
---
+++ src/FilmRow.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class FilmRow extends Component {
+  render() {
+    return (
+      )
+  }
+}
+
